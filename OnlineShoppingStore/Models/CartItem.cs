f4734afdbf5f596f7178b05007c248cc44f60252
Tasneem227 <138536495+Tasneem227@users.noreply.github.com>
2025-09-12 22:44:46 +0300

    Models1

--- OnlineShoppingStore/Models/CartItem.cs
+++ OnlineShoppingStore/Models/CartItem.cs
@@ -2,4 +2,10 @@
 
 public class CartItem
 {
+    public int Id { get; set; }
+    public int Quantity { get; set; }
+    public int ProductId { get; set; }
+    public Product Product { get; set; }
+    public int CartId { get; set; }
+    public Cart Cart { get; set; }
 }
--- OnlineShoppingStore/Models/Category.cs
+++ OnlineShoppingStore/Models/Category.cs
@@ -2,4 +2,9 @@
 
 public class Category
 {
+    public int CategoryId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public ICollection<Admin>Admins { get; set; } = new List<Admin>();
+    public ICollection<Product> Products { get; set; } = new List<Product>();
 }
