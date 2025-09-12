c33aff9a7fb6d54295d23ffa23c009bdfbb67f4a
shahd0311 <shahd@SHAHDMAHMOUD>
2025-09-12 23:21:44 +0300

    models3

--- OnlineShoppingStore/Models/Discount.cs
+++ OnlineShoppingStore/Models/Discount.cs
@@ -2,4 +2,18 @@
 
 public class Discount
 {
+    public int DiscountId { get; set; }
+    public string Name { get; set; }
+    public string Code { get; set; }
+    public decimal Percentage { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public bool IsActive { get; set; }
+
+    public ICollection<Admin>? Admins = new List<Admin>();
+
+    public ICollection<Product>? Products = new List<Product>();
+
+
 }
--- OnlineShoppingStore/Models/Order.cs
+++ OnlineShoppingStore/Models/Order.cs
@@ -2,4 +2,19 @@
 
 public class Order
 {
+    public int OrderId { get; set; }
+    public DateTime OrderDate { get; set; }
+    public string Status { get; set; }
+    public decimal TotalAmount { get; set; }
+
+    public ICollection<OrderItem>? OrderItems = new List<OrderItem>();
+
+    public ICollection<Product>? Products = new List<Product>();
+
+    public int CustomerId { get; set; }
+    public Customer Customer { get; set; }
+
+
+
+
 }
